8f59934a5c489bb57320c46843340ee8ae49d22f
Masone1811 <Masone1811@gmail.com>
2026-09-08 16:19:18 -0600

    Added timer and changed mob spawn rate
    Added a timer to the top right of the screen that tracks how long the player has survived for and changed the mob spawn rate to start slower but increase based on survival time.

--- survivor_game.gd
+++ survivor_game.gd
@@ -1,6 +1,20 @@
 extends Node2D
 
 
+var survival_time: float = 0.0
+var spawn_time: float = 2.0
+var min_spawn_time: float = 0.5
+var spawn_rate: float = .01
+
+
+func _process(delta):
+	survival_time += delta
+	
+	var minutes = int(survival_time) / 60
+	var seconds = int(survival_time) % 60
+	
+	%SurvivalTimer.text = "%02d:%02d" % [minutes, seconds]
+
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
@@ -10,6 +24,10 @@ func spawn_mob():
 
 func _on_timer_timeout():
 	spawn_mob()
+	
+	var new_spawn_time = spawn_time - (survival_time * spawn_rate)
+	
+	%Timer.wait_time = max(new_spawn_time, min_spawn_time)
 
 
 func _on_player_health_depleted() -> void:
