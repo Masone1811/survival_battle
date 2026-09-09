451643bb7062d374b7ed9488e17313cb1b865707
Masone1811 <Masone1811@gmail.com>
2026-09-08 20:49:18 -0600

    Fixed camera and screen wrap
    I added a Camera2D node and fixed the position to the center of the screen so that the camera remains stationary, and I added a function that allows the player to wrap around to the other side of the screen when they reach the borders of the screen.

--- player.gd
+++ player.gd
@@ -4,17 +4,19 @@ signal health_depleted
 
 var health = 100.0
 
+var screen_size = Vector2(1920, 1080)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction * 600
 	move_and_slide()
+	screen_wrap()
 	
 	if velocity.length() > 0.0:
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
-
+	
 	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
@@ -23,3 +25,13 @@ func _physics_process(delta):
 		if health <= 0.0:
 			health_depleted.emit()
 			
+
+func screen_wrap():
+	if position.x > screen_size.x:
+		position.x = 0
+	if position.x < 0:
+		position.x = screen_size.x
+	if position.y > screen_size.y:
+		position.y = 0
+	if position.y < 0:
+		position.y = screen_size.y
